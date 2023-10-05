08207894bcc37f5fd110f89d0274bf1dfb9f9f16
AmaliaSeptiani <139928149+AmaliaSeptiani@users.noreply.github.com>
2023-10-06 06:41:20 +0700

    Update BMIcalculator.js

--- BMIcalculator.js
+++ BMIcalculator.js
@@ -1,18 +1,22 @@
 document.getElementById('card').addEventListener('submit', function(e){
-    e.preventDefault()
-    var height = document.getElementById('height')
-    var weight = document.getElementById('weight')
-    var h = height.value
-    var w = weight.value
-    var hasil = w/(h/100)^2
-    if(hasil<18.5){
-        var categories = 'Underweight'
-    }else if(hasil = 18.5 && 24.9){
-        var categories = 'Normal Weight'
-    }else if(hasil = 25 && 29.9){
-        var categories = 'Overweight'
-    }else{
-        var categories = 'Obesity'
+    e.preventDefault();
+    var height = document.getElementById('height');
+    var weight = document.getElementById('weight');
+    var h = height.value;
+    var w = weight.value;
+    var hasil = w / (h / 100) ** 2;
+
+    var categories;
+
+    if (hasil < 18.5) {
+        categories = 'Underweight';
+    } else if (hasil >= 18.5 && hasil <= 24.9) {
+        categories = 'Normal Weight';
+    } else if (hasil >= 25 && hasil <= 29.9) {
+        categories = 'Overweight';
+    } else {
+        categories = 'Obesity';
     }
-    document.getElementById('hasil').innerText='Your BMI is '+hasil+' which mean You are '+categories
-})
+
+    document.getElementById('hasil').innerText = 'Your BMI is ' + hasil.toFixed(2) + ' which means you are ' + categories;
+});
